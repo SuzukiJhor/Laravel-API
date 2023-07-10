66821abc27faeb19e18b6bd30a5a66bfa17a157d
jhordan.followize <jhordan.suzuki@followize.com.br>
2023-07-10 18:04:25 -0300

    feat: Implementando funcionalidade de editar usuário

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Editar Usuário')
+
+@section('content')
+
+<h1 class="w-full text-3xl text-black pb-6">Editar Usuário {{ $this->name }}</h1>
+
+        <div class="flex flex-wrap">
+            <div class="w-full my-6 pr-0 lg:pr-2">
+                <div class="leading-loose">
+                    <form class="p-10 bg-white rounded shadow-xl" action="{{ route('users.update') }}" method="POST">
+                        @method('PUT')
+                       @include('admin.users._partials.form')
+                    </form>
+                </div>
+            </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,10 @@ use App\Http\Controllers\Admin\{
 Route::group(['middleware' => 'web'], function () {
     
     Route::prefix('admin')->group(function () {
-    
+        
         Route::post('users', [UserController::class,'store'])->name('users.store');
         Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::get('/users/{id}', [UserController::class, 'edit'])->name('users.edit');
         Route::get('/users', [UserController::class, 'index'])->name('users.index');
         
         Route::get('/', [AdminController::class, 'index'])-> name('admin.home');
